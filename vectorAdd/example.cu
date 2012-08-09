1131b8662b43746973a13dcee9a96b3a5ab00830
jantogni <jantogni@r480.(none)>
2012-08-09 15:21:55 -0400

    vectorAdd modifications

--- vectorAdd/example.cu
+++ vectorAdd/example.cu
@@ -3,26 +3,30 @@
 #include <cuda.h>
 #include <iostream>
 
-#define N 60
+#define N 3000
 
 using namespace std;
 
 __global__ void add(int *a, int *b, int *c) {
-	int i = blockIdx.x;
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if(i<N)
 		c[i] = a[i] + b[i];
 }
 
 int main() {
-	cudaDeviceProp prop;
-	int a[N], b[N], c[N];	
-	int *dev_a, *dev_b, *dev_c;
+	int *a,		*b,		*c;
+	int *dev_a,	*dev_b,		*dev_c;
+	int i;
+
+	a = (int *)malloc(N*sizeof(int));
+	b = (int *)malloc(N*sizeof(int));
+	c = (int *)malloc(N*sizeof(int));
 
 	cudaMalloc( (void**)&dev_a, N * sizeof(int) );
 	cudaMalloc( (void**)&dev_b, N * sizeof(int) );
 	cudaMalloc( (void**)&dev_c, N * sizeof(int) );
 
-	for( int i=0; i<N; i++){
+	for(i=0; i<N; i++){
 		a[i] = i;
 		b[i] = 2*i;
 	}
@@ -30,19 +34,14 @@ int main() {
 	cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
 	
-	add<<<N,1>>>(dev_a, dev_b, dev_c);
+	add<<< ceil(N/512.0),512>>>(dev_a, dev_b, dev_c);
 
 	cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+
+	for(i=0; i<N; i++){
+		cout << a[i] << " + " << b[i] << " = " << c[i] << endl;
+	}
 	
-
-	//for( int i=0; i<N; i++){
-	//	cout << a[i] << " " << b[i] << " " << c[i] << endl;
-	//}
-
-	cudaGetDeviceProperties(&prop,0);
-
-	cout << prop.maxThreadsPerBlock << endl;
-
 	cudaFree(dev_a);
 	cudaFree(dev_b);
 	cudaFree(dev_c);
